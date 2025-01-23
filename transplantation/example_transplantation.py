d6c71e21e64a4169c9d8b7cef706f80260ab28ac
sofiesana <119871294+sofiesana@users.noreply.github.com>
2025-01-23 20:45:44 +0100

    for extra experiment

--- transplantation/example_transplantation.py
+++ transplantation/example_transplantation.py
@@ -7,6 +7,7 @@ from utils import get_image
 from ImageWithTransplantedObjects import ImageWithTransplantedObjects
 from ExtractedObject import ExtractedObject
 import os
+import pickle 
 
 
 def main():
@@ -22,6 +23,7 @@ def main():
     dataset.persistent = True
 
     sample = dataset.first()
+
     # obj_extract = ImageObjectExtractor(sample, save_location=save_location, object_log_file='transplantation/outputs/extracted_objects_log.json')
     # print("extracting objects")
     # obj_extract.extract_objects()
@@ -32,6 +34,14 @@ def main():
     # obj.display_extracted_object()
     new_image.add_transplanted_object(obj, (0,0))
     # new_image.add_transplanted_object(obj, (200, 200))
+    new_image.display_transplanted_image()
+    new_image.save_transplanted_image()
+
+    new_image = ImageWithTransplantedObjects(sample=sample, save_location=save_location, dataset_name="testing")
+    obj = ExtractedObject(log_file_path='transplantation/outputs/extracted_objects_log.json')
+    obj.load_object(os.path.join(save_location, f'extracted_objects/person_{sample.id}.pkl'))
+    new_image.add_transplanted_object(obj, (0,0))
+    new_image.add_transplanted_object(obj, (200, 200))
     new_image.display_transplanted_image()
     new_image.save_transplanted_image()
 
