67ac03876939545153202a9387669f131452d17e
sofiesana <119871294+sofiesana@users.noreply.github.com>
2025-01-23 21:36:37 +0100

    fixing namings, saving the new samples and dataset

--- transplantation/ImageWithTransplantedObjects.py
+++ transplantation/ImageWithTransplantedObjects.py
@@ -10,13 +10,13 @@ import json
 import fiftyone as fo
 
 class ImageWithTransplantedObjects():
-  def __init__(self, sample, save_location, dataset_name, log_file="transplantation_log.json"):
-    self.log_file = dataset_name + "_" + log_file
+  def __init__(self, sample, save_location, dataset_name):
+    self.log_file = dataset_name + "_" + "transplantation_log.json"
     original_image_path = sample.filepath
     if save_location is None:
       self.save_location = ''
     else:
-      self.save_location = save_location
+      self.save_location = os.path.join(save_location, dataset_name)
     self.og_image = Image.open(original_image_path)
     self.modified_image = self.og_image
     self.og_id = sample.id
@@ -38,8 +38,19 @@ class ImageWithTransplantedObjects():
 
     self.modified_sample = fo.Sample(filepath=self.image_save_location)
     self.setup_modified_sample()
+
+    self.dataset = self.setup_dataset()
+
+  def setup_dataset(self):
+    if self.dataset_name not in fo.list_datasets():
+      dataset = fo.Dataset(name=self.dataset_name)
+    else:
+      dataset = fo.load_dataset(self.dataset_name)
+    dataset.persistent = True
+    return dataset
   
   def setup_modified_sample(self):
+    self.modified_sample["new_id"] = self.transplanted_image_id
     self.modified_sample["ground_truth"] = self.og_sample["ground_truth"]
     self.modified_sample.id = self.transplanted_image_id
     self.modified_sample.metadata = self.og_sample.metadata
@@ -55,7 +66,6 @@ class ImageWithTransplantedObjects():
   def save_transplanted_image(self):
     self.log_modified_image()
     self.save_image()
-    # self.save_sample_to_json()
 
   def log_modified_image(self):
     entry = {
@@ -66,15 +76,20 @@ class ImageWithTransplantedObjects():
         "transplantations": self.transplantations
        }
     }
-
     log_entry(self.log_file, entry, self.og_id)
 
   def save_image(self):
     image = Image.fromarray(self.modified_image)
     image.save(self.image_save_location)
+    
+    self.dataset.add_sample(self.modified_sample)
+    self.modified_sample["original_image_id"] = self.og_id
+    self.modified_sample["origina_image_path"] = self.og_sample.filepath
+    self.modified_sample.save()
 
-    with open(self.modified_sample_path, 'wb') as f:
-        pkl.dump(self.modified_sample, f)
+    json_sample = self.modified_sample.to_dict(include_private=True)
+    with open(self.modified_sample_path, 'w') as f:
+      json.dump(json_sample, f)
 
   def display_transplanted_image(self):
     image = Image.fromarray(self.modified_image)
@@ -82,16 +97,17 @@ class ImageWithTransplantedObjects():
   
   def update_modified_sample_with_transplant(self, obj, location):
         print("updating modified sample")
-        # note, this does not actually update the sample image, just the bounding boxes...
-
-        # Calculate the new bounding box and segmentation map
         new_bbox = [location[0], location[1], obj.box[2], obj.box[3]]
         new_segmentation = obj.mask
-
-        # Add the new detection to the sample
         new_detection = {
             "label": obj.class_label,
             "bounding_box": new_bbox,
             "mask": new_segmentation
         }
+        new_detection = fo.Detection(
+                    label=obj.class_label,
+                    bounding_box=new_bbox,
+                    mask=new_segmentation
+                )
         self.modified_sample["ground_truth"].detections.append(new_detection)
+        
