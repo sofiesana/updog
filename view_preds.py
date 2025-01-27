9770c5ea42e7ce8cd8e91e40a0036a1cd947d4ba
sofiesana <119871294+sofiesana@users.noreply.github.com>
2025-01-27 23:21:50 +0100

    to see acc

--- view_preds.py
+++ view_preds.py
@@ -6,21 +6,29 @@ def show_predictions(ds_og_name, ds_trans_name, model_name, dataset_to_view, max
     ds_trans = fo.load_dataset(ds_trans_name)
 
     model = foz.load_zoo_model(model_name)
+    ds_og.apply_model(model, label_field="predictions")
+    results = ds_og.evaluate_detections(
+        "predictions",
+        gt_field="ground_truth",
+        eval_key="eval",
+    )
+
+    print(results.metrics())
 
     # Idk if this is needed cuz the predictions are already applied in the dataset
     # But they are based on the last model applied, so this can be used to change the model
-    for og_idx, sample in enumerate(ds_og):
-        pred_og = ds_og.match({"filepath": sample.filepath})
-        pred_og.apply_model(model, label_field="predictions")
+    # for og_idx, sample in enumerate(ds_og):
+    #     pred_og = ds_og.match({"filepath": sample.filepath})
+    #     pred_og.apply_model(model, label_field="predictions")
 
-        for trans_idx, trans_sample in enumerate(ds_trans):
-            if trans_sample.original_image_id == sample.id:
+    #     for trans_idx, trans_sample in enumerate(ds_trans):
+    #         if trans_sample.original_image_id == sample.id:
 
-                pred_trans = ds_trans.match({"filepath": sample.filepath})
-                pred_trans.apply_model(model, label_field="predictions")
+    #             pred_trans = ds_trans.match({"filepath": sample.filepath})
+    #             pred_trans.apply_model(model, label_field="predictions")
 
-        if og_idx >= max_images:
-            break
+    #     if og_idx >= max_images:
+    #         break
 
     if dataset_to_view == "og":
         # View the predictions using the FiftyOne App
@@ -36,13 +44,13 @@ def show_predictions(ds_og_name, ds_trans_name, model_name, dataset_to_view, max
 
 if __name__ == '__main__':
     ds_og = 'coco-2017-validation-25'
-    ds_trans = 'transdata_20_n25'
+    ds_trans = 'transdata_0_n25'
 
-    models_to_test = ['yolov8m-world-torch'] # ['rtdetr-l-coco-torch']
+    models_to_test =  ['rtdetr-l-coco-torch']# ['yolov8m-world-torch'] # 
 
     # dataset_to_view = "og" 
-    dataset_to_view = "trans" 
+    dataset_to_view = "og" 
 
     for model in models_to_test:
-        show_predictions(ds_og, ds_trans, model, dataset_to_view, max_images=2)
+        show_predictions(ds_og, ds_trans, model, dataset_to_view, max_images=25)
     
